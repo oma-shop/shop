ef4e492646cda4d4e12b99c6224382121e554874
huhn511 <sehe89@gmail.com>
2020-03-26 04:02:15 +0100

    add static content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,27 @@
+require('dotenv').config()
+const config = require('./config.json')
+
+const title = process.env.TITLE ? process.env.TITLE : config.title;
+const description = process.env.DESCRIPTION ? process.env.DESCRIPTION : config.description;
+const gh_repo_name = process.env.GH_REPO_NAME ? process.env.GH_REPO_NAME : config.gh_repo_name;
+const latitude = process.env.LATITUDE ? process.env.LATITUDE : config.location.latitude;
+const longitude = process.env.LONGITUDE ? process.env.LONGITUDE : config.location.longitude;
+
 
 module.exports = {
   mode: 'universal',
+  router: {
+    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/' + gh_repo_name + '/' : ''
+  },
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: title + ' Shop - Open Marketplace Application}',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: description}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -45,12 +57,47 @@ module.exports = {
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
+    'nuxt-i18n',
+    'nuxt-leaflet',
   ],
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        name: 'English'
+      },
+      {
+        code: 'de',
+        name: 'Deutsch'
+      }
+    ],
+    defaultLocale: 'en',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      alwaysRedirect: true
+    },
+    vueI18n: {
+      fallbackLocale: 'en',
+      messages: {
+        en: require('./translations/en.js'),
+        de: require('./translations/de.js')
+      }
+    }
+  },
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+  },
+  env: {
+    shopUrl: process.env.URL || 'http://localhost:5000',
+    title: title,
+    shopDescription: description,
+    heroImageUrl: process.env.HERO_IMGAE_URL || '',
+    shopLatitude: latitude,
+    shopLongitude: longitude,
   },
   /*
   ** Build configuration
